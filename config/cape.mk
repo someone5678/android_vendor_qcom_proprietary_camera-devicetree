cf08455356e76a08b5c98587d5cc26cbbc08bbd0
Stephen Sinn <stevexian56@gmail.com>
2023-03-11 15:31:04 +0800

    Import Xiaomi camera device tree changes for diting
    Signed-off-by: Stephen Sinn <stevexian56@gmail.com>

--- config/cape.mk
+++ config/cape.mk
@@ -2,7 +2,7 @@ dtbo-$(CONFIG_ARCH_WAIPIO) := waipio-camera.dtbo
 dtbo-$(CONFIG_ARCH_WAIPIO) += waipio-camera-overlay-v2.dtbo \
 								waipio-camera-sensor-mtp.dtbo \
 								waipio-camera-sensor-cdp.dtbo \
-								waipio-camera-sensor-qrd.dtbo \
+								waipio-camera-sensor-qrd.dtbo
 
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera.dtbo
 dtbo-$(CONFIG_ARCH_DIWALI) += diwali-camera-sensor-idp.dtbo
@@ -12,3 +12,4 @@ dtbo-$(CONFIG_ARCH_CAPE) += cape-camera.dtbo
 dtbo-$(CONFIG_ARCH_CAPE) += cape-camera-sensor-mtp.dtbo \
 				cape-camera-sensor-cdp.dtbo \
 				cape-camera-sensor-qrd.dtbo \
+				diting-sm8475-camera-sensor.dtbo
